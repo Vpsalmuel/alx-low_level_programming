a13c68c3e3023d861e6ace4b7b1622a190b983d2
Vpsalmuel <vpsalmuel@gmail.com>
2022-03-10 10:21:54 -0800

    second commits

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  *  main - Prints String use puts instead of printf
+ *
+ *   *  Description: "\"Programming is like building a multilingual puzzle"
+ *
+ *  *  Programme  Return: 0
+ *
+ *     */
+
+
+
+int main(void)
+
+
+
+{
+
+
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+
+
+}
